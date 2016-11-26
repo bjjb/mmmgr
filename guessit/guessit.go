18fdf505c4862df34cd62fa3c6b51398a739aeff
JJ Buckley <jj@bjjb.org>
2016-11-26 23:03:21 +0100

    Final refactoring of guessit
    Now the library will complain if there's no way to run. Which is ok, for now.
    Included are some basic test cases.

--- guessit/guessit.go
+++ guessit/guessit.go
@@ -1,43 +1,67 @@
+/*
+guessit provides a wrapper around toilal/guessit, a python library for
+guessing video-file names.
+*/
+
 package guessit
 
 import (
 	"log"
 	"os/exec"
+	"strings"
 )
 
-type json []byte
+func Guessit(path string) []byte {
+	return guessit(path)
+}
 
+var cache map[string][]byte
 var cmd []string
-var cache map[string]json
+var function func(string) []byte
 
 func init() {
-	cache = make(map[string]json)
-	if _, err := exec.LookPath("guessit"); err == nil {
-		cmd = []string{"guessit", "-j"}
-		return
+	cache = make(map[string][]byte)
+	switch {
+	case isCommand("guessit"):
+		function = func(path string) []byte {
+			return execute("guessit -j", path)
+		}
+	case isCommand("docker"):
+		function = func(path string) []byte {
+			return execute("docker run --rm toilal/guessit -j", path)
+		}
+	default:
+		log.Fatalf("Failed to initialize guessit.")
 	}
-	if _, err := exec.LookPath("docker"); err == nil {
-		cmd = []string{"docker", "run", "--rm", "toilal/guessit", "-j"}
-		return
-	}
-	log.Fatal(`Failed to find guessit or docker on the system.
-If you have python and pip (or pip3), install it with 'pip install guessit'.
-Otherwise install docker on your system, and I can use a docker image
-instead.`)
 }
 
-func guessit(path string) (json, error) {
-	return exec.Command(cmd[0], append(cmd[1:], path)...).Output()
+// Runs guessit (or guessit in a docker)
+func guessit(path string) []byte {
+	// return immediately if the result was cached
+	if v, found := cache[path]; found {
+		return v
+	}
+	result := function(path)
+	cache[path] = result
+	return result
 }
 
-func Guessit(path string) json {
-	if j, found := cache[path]; found {
-		return j
-	}
-	j, err := guessit(path)
+// Utility function to test whether a command exists on the system
+func isCommand(cmd string) bool {
+	_, err := exec.LookPath(cmd)
+	return err == nil
+}
+
+// Utility function to execute a command and return the output. Splits the cmd
+// and uses the first one as the actual command to exec.Command, and the rest
+// (including the args) as arguments.
+func execute(argc string, argv ...string) []byte {
+	args := append(strings.Split(argc, " "), argv...)
+	argc = args[0]
+	argv = args[1:]
+	out, err := exec.Command(argc, argv...).Output()
 	if err != nil {
-		log.Fatalf("guessit error: %q", err)
+		log.Fatalf("error from guessit: %q", err)
 	}
-	cache[path] = j
-	return j
+	return out
 }
--- guessit/guessit_test.go
+++ guessit/guessit_test.go
@@ -1,13 +1,22 @@
 package guessit
 
 import (
+	"strings"
 	"testing"
 )
 
-func TestGuessit(t *testing.T) {
-	t.Run("guessit", func(t *testing.T) {
-		if _, err := guessit("Hello World"); err != nil {
-			t.Fatalf("Error from guessit: %v", err)
+func Test_guessit(t *testing.T) {
+	squeezer := strings.NewReplacer(" ", "", "\n", "")
+	tests := []struct{ in, ok string }{
+		{"hello", `{"title":"hello","type":"movie"}`},
+		{"hello.1999", `{"title":"hello","year":1999,"type":"movie"}`},
+		{"hello.world.1999[yarbag].HDTV.XviD.mkv", `{"title":"helloworld","year":1999,"release_group":"yarbag","format":"HDTV","video_codec":"XviD","container":"mkv","mimetype":"video/x-matroska","type":"movie"}`},
+		{"s01e01.hello.1999", `{"season":1,"episode":1,"title":"hello","year":1999,"type":"episode"}`},
+	}
+	for _, c := range tests {
+		out := squeezer.Replace(string(guessit(c.in)))
+		if out != c.ok {
+			t.Errorf("Guessit(%q): expected %q, got %q", c.in, c.ok, out)
 		}
-	})
+	}
 }
