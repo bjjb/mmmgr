c9d4056d24998c619a933f7be3ec3128ef865409
JJ Buckley <jjbuckley@gmail.com>
2017-03-10 02:52:03 +0100

    Moves some TVDB client logic into separate files.

---
+++ tvdb/language.go
@@ -0,0 +1,11 @@
+package tvdb
+
+/*
+A Language encapsulates a language supported by The TVDB.
+*/
+type Language struct {
+	ID          int    `json:"id"`
+	Abbr        string `json:"abbreviation"`
+	Name        string `json:"name"`
+	EnglishName string `json:"englishName"`
+}
